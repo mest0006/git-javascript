8bb5e7f51c939b9b41cc837edfd3c863c4b2bfc4
Borbala Mester <mest0006@algonquinlive.com>
2020-01-08 20:29:22 -0500

    javascript commited

---
+++ script.js
@@ -0,0 +1,35 @@
+var name = "borbala";
+var number = "2";
+console.log("hello my name is " + name);
+console.log(number * "2" * "5" / number - "7");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
